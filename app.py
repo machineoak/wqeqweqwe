c592c5001dfcc4f5ec9163eb5702d7a93197e17c
keven carvalho <dangerdasedicoes@gmail.com>
2025-09-13 10:50:40 -0300

    put

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
 
 @app.get("/")
 async def read_root():
@@ -14,3 +20,7 @@ async def read_item(item_id: str, q: Union[str, None] = None):
 @app.get("/items/num/{item_num}")
 async def read_item(item_num: int, q: Union[int, None] = None):
     return {item_num: item_num, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
